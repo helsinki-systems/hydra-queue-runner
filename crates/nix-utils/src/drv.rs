cec0ca63574819d10e2d7a5fde01b531da0788c0
Simon Hauser <simon.hauser@helsinki-systems.de>
2025-09-26 15:42:57 +0200

    feat(nix_utils): is_ca and get_ca_output helper

--- crates/nix-utils/src/drv.rs
+++ crates/nix-utils/src/drv.rs
@@ -11,6 +11,14 @@ pub struct Output {
 }
 
 #[derive(Debug, Clone)]
+pub struct CAOutput {
+    pub name: String,
+    pub path: StorePath,
+    pub hash: String,
+    pub hash_algo: String,
+}
+
+#[derive(Debug, Clone)]
 pub struct DerivationEnv {
     inner: AHashMap<String, String>,
 }
@@ -88,6 +96,30 @@ impl Derivation {
             system: String::from_utf8(v.platform).unwrap_or_default(),
         })
     }
+
+    pub fn is_ca(&self) -> bool {
+        self.outputs
+            .iter()
+            .any(|o| o.hash.is_some() && o.hash_algo.is_some())
+    }
+
+    pub fn get_ca_output(&self) -> Option<CAOutput> {
+        self.outputs.iter().find_map(|o| {
+            Some(CAOutput {
+                path: o.path.clone()?,
+                hash: o.hash.clone()?,
+                hash_algo: o.hash_algo.clone()?,
+                name: o.name.clone(),
+            })
+        })
+    }
+}
+
+fn parse_drv(path: String, input: &str) -> Result<Derivation, crate::Error> {
+    Ok(Derivation::new(
+        path,
+        nix_diff::parser::parse_derivation_string(input)?,
+    )?)
 }
 
 #[tracing::instrument(fields(%drv), err)]
@@ -102,8 +134,32 @@ pub async fn query_drv(drv: &StorePath) -> Result<Option<Derivation>, crate::Err
     }
 
     let input = tokio::fs::read_to_string(&full_path).await?;
-    Ok(Some(Derivation::new(
-        full_path,
-        nix_diff::parser::parse_derivation_string(&input)?,
-    )?))
+    Ok(Some(parse_drv(full_path, &input)?))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{StorePath, drv::parse_drv};
+
+    #[test]
+    fn test_ca_derivation() {
+        let drv_str = r#"Derive([("out","/nix/store/6fr8dalasgpy0bpykhjq2b9q65lb4j8y-linux-6.16.tar.xz","sha256","1a4be2fe6b5246aa4ac8987a8a4af34c42a8dd7d08b46ab48516bcc1befbcd83")],[],[],"builtin","builtin:fetchurl",[],[("builder","builtin:fetchurl"),("executable",""),("impureEnvVars","http_proxy https_proxy ftp_proxy all_proxy no_proxy"),("name","linux-6.16.tar.xz"),("out","/nix/store/6fr8dalasgpy0bpykhjq2b9q65lb4j8y-linux-6.16.tar.xz"),("outputHash","sha256-Gkvi/mtSRqpKyJh6ikrzTEKo3X0ItGq0hRa8wb77zYM="),("outputHashAlgo",""),("outputHashMode","flat"),("preferLocalBuild","1"),("system","builtin"),("unpack",""),("url","https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.16.tar.xz"),("urls","https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.16.tar.xz")])%"#;
+        let drv = parse_drv(
+            "/nix/store/awmdz2lkxkdqnhdhk09zy9w7kzpl8jhc-linux-6.16.tar.xz.drv".into(),
+            drv_str,
+        )
+        .unwrap();
+        assert!(drv.is_ca());
+        let o = drv.get_ca_output().unwrap();
+        assert_eq!(
+            o.path,
+            StorePath::new("/nix/store/6fr8dalasgpy0bpykhjq2b9q65lb4j8y-linux-6.16.tar.xz")
+        );
+        assert_eq!(o.name, String::from("out"));
+        assert_eq!(
+            o.hash,
+            String::from("1a4be2fe6b5246aa4ac8987a8a4af34c42a8dd7d08b46ab48516bcc1befbcd83")
+        );
+        assert_eq!(o.hash_algo, String::from("sha256"));
+    }
 }
