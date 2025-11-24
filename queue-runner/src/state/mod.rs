aa8c0a727a5f0ff059d50db5e0eca65fd740f261
Simon Hauser <simon.hauser@helsinki-systems.de>
2025-11-24 11:37:50 +0100

    feat: make queue clonable

--- queue-runner/src/state/mod.rs
+++ queue-runner/src/state/mod.rs
@@ -880,7 +880,7 @@ impl State {
                 )
                 .await;
         }
-        self.queues.remove_all_weak_pointer().await;
+        self.queues.remove_all_weak_pointer(None).await;
 
         let nr_steps_waiting_all_queues = self
             .queues
@@ -1967,7 +1967,9 @@ impl State {
                     .remove_job_by_path(step.get_drv_path(), QueueType::Main)
                     .await;
             }
-            self.queues.remove_all_weak_pointer().await;
+            self.queues
+                .remove_all_weak_pointer(Some(QueueType::Main))
+                .await;
         }
         self.metrics.nr_unsupported_steps.set(count);
         self.metrics
