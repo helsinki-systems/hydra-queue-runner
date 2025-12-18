81d85b98eac10261f7d4b3925f81fd85d79d6659
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-12-18 19:24:43 +0100

    fix: update postgresql port reference
    Follows the rename into the settings attribute.

--- runner-module.nix
+++ runner-module.nix
@@ -280,7 +280,7 @@ in
         StateDirectoryMode = "0700";
         ReadWritePaths = [
           "/nix/var/nix/gcroots/"
-          "/run/postgresql/.s.PGSQL.${toString config.services.postgresql.port}"
+          "/run/postgresql/.s.PGSQL.${toString config.services.postgresql.settings.port}"
           "/nix/var/nix/daemon-socket/socket"
           "/var/lib/hydra/build-logs/"
         ];
