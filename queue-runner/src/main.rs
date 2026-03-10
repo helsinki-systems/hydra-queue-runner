5ffef8948e3f8ccbfa44fd7a60d5ac1b4fb41811
Simon Hauser <simon.hauser@helsinki-systems.de>
2026-03-10 17:26:47 +0100

    fix: clear busy state on startup

--- queue-runner/src/main.rs
+++ queue-runner/src/main.rs
@@ -99,6 +99,8 @@ async fn main() -> anyhow::Result<()> {
     let _lock = lock_file::LockFile::acquire(&lockfile_path)
         .context("Another instance is already running.")?;
 
+    state.clear_busy().await?; // clear busy once before starting the queue-runner
+
     if !state.cli.mtls_configured_correctly() {
         tracing::error!(
             "mtls configured inproperly, please pass all options: server_cert_path, server_key_path and client_ca_cert_path!"
